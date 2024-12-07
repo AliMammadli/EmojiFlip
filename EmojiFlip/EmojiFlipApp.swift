6f1e7f0486a201f68adb3bf4ea2e868979e91cca
Ali Mammadli <nfsavss@icloud.com>
2024-12-07 15:17:35 +0400

    Initial Commit

---
+++ EmojiFlip/EmojiFlipApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiFlipApp.swift
+//  EmojiFlip
+//
+//  Created by Ali Mammadli on 07.12.24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiFlipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
