bdfad9f78cbabb035b92eb2e4cc04c62e0d21865
Ali Mammadli <nfsavss@icloud.com>
2024-12-08 22:45:00 +0400

    Separated body views

--- EmojiFlip/ContentView.swift
+++ EmojiFlip/ContentView.swift
@@ -8,15 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis = ["👻", "🎃", "🕷️", "😈", "💀"]
+    let emojis = ["👻", "🎃", "🕷️", "😈", "💀", "🕸️", "🧙🏻‍♀️", "🙀", "👹", "😱", "☠️", "🍭"]
+    @State var cardCount = 4
     
     var body: some View {
         VStack {
-            ForEach(emojis.indices, id: \.self) { index in
-                CardView(content: emojis[index])
+            ScrollView {
+                cards
             }
+            Spacer()
+            cardCountControls
+            .font(.largeTitle)
+            .padding()
         }
         .padding()
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+        }
+    }
+    
+    var cardCountControls: some View {
+        HStack {
+            cardRemoveBtn
+            Spacer()
+            cardAddBtn
+        }
+    }
+    
+    func cardCountController(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardRemoveBtn: some View {
+        cardCountController(by: -1, symbol: "rectangle.stack.fill.badge.minus")
+    }
+    
+    var cardAddBtn: some View {
+        cardCountController(by: 1, symbol: "rectangle.stack.fill.badge.plus")
     }
 }
 
@@ -26,21 +65,22 @@ struct CardView: View {
     
     var body: some View {
         ZStack {
-            if isOpened {
+            Group {
                 RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 5)
+                    .strokeBorder(lineWidth: 2)
                     .foregroundColor(.pink)
                     .onTapGesture {
                         isOpened.toggle()
                     }
                 Text(content).font(.largeTitle)
-            } else {
-                RoundedRectangle(cornerRadius: 12)
-                    .fill(.pink)
-                    .onTapGesture {
-                        isOpened.toggle()
-                    }
             }
+            .opacity(isOpened ? 1 : 0)
+            RoundedRectangle(cornerRadius: 12)
+                .fill(.pink)
+                .opacity(isOpened ? 0 : 1)
+                .onTapGesture {
+                    isOpened.toggle()
+                }
         }
     }
 }
